3130b4daf2a5260d01c49a009f91e795c9e94338
ThanhPhat1509 <161999845+ThanhPhat1509@users.noreply.github.com>
2024-03-13 20:18:56 +0700

    Add files via upload

---
+++ Project_class..cpp
@@ -0,0 +1,89 @@
+#include "struct_and_library.h"
+Class CreateClass(string classid,string headteacher,StudentNode* studenthead)
+{
+	Class newclass;
+	newclass.class_id = classid;
+	newclass.class_id = headteacher;
+	newclass.student_list = studenthead;
+	return newclass;
+}
+ClassNode* InitializeClassNode(Class newclass) 
+{
+	ClassNode* newclassnode = new ClassNode();
+	newclassnode->my_class = newclass;
+	newclassnode->next = nullptr;
+	return newclassnode;
+}
+void AddClassNode(ClassNode* &head,Class newclass)
+{
+	ClassNode* newclassnode = new ClassNode(); 
+	newclassnode = InitializeClassNode(newclass);
+	if (!head) 
+	{
+		head = newclassnode;
+	}
+	else 
+	{
+		ClassNode* List_Class = head;
+		while (List_Class->next)
+		{
+			List_Class = List_Class->next;
+		}
+		List_Class->next = newclassnode;
+	}
+}
+void DeleteClassNode(ClassNode* &head,Class del_class)
+{
+	if (!head) 
+	{
+		cout << "Invalid.\n";
+		return;
+	}
+	ClassNode* tmp = nullptr;
+	if (head->my_class.class_id == del_class.class_id)
+	{
+		tmp = head;
+		head = head->next;
+		delete tmp;
+	}
+	else 
+	{
+		tmp = head;
+		while (tmp->next) 
+		{
+			if (tmp->next->my_class.class_id == del_class.class_id) 
+			{
+				ClassNode* delnode = tmp->next;
+				tmp->next = tmp->next->next;
+				delete delnode;
+				return;
+			}
+			else
+			{
+				tmp = tmp->next;
+			}
+		}
+		cout << "the class that need to remove don't exist.\n";
+	}
+}
+ClassNode* SearchClassNode(ClassNode*& head,Class searchclass) 
+{
+	if (!head) 
+	{
+		cout << "the class that need to find don't exist.\n";
+		return nullptr;
+	}
+	else 
+	{
+		ClassNode* tmp = head;
+		while (tmp)
+		{
+			if (tmp->my_class.class_id == searchclass.class_id)
+				return tmp;
+			else
+				tmp = tmp->next;
+		}
+		cout << "the class that need to find don't exist.\n";
+		return nullptr;
+	}
+}
