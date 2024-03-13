f5f2ff6bea29427cf19a1e485b814bbafa585cb9
NamHV05 <88269724+NamHV05@users.noreply.github.com>
2024-03-13 12:42:17 +0700

    Update semester.cpp

--- semester/semester/semester.cpp
+++ semester/semester/semester.cpp
@@ -14,19 +14,22 @@ void createSemester(YearNode*& pHY)
 {
 	cout << "You are creating a new semester." << endl;
 	//Missing enter 0 to go back...
+	//Enter semester and school year
 	int sem;
 	cout << "Enter semester number: ";
 	cin >> sem;
 	string semester_id = "Semester " + to_string(sem);
 
 	int start, end;
-	cout << "Enter schoolyear:" << endl
+	cout << "Enter school year:" << endl
 		<< "	Enter start year: ";
 	cin >> start;
 	cout << "	Enter end year: ";
 	cin >> end;
 	string year_id = to_string(start) + " - " + to_string(end);
 	
+	//Check and add semester
+	YearNode* currY = pHY;
 	if (!pHY)
 	{
 		pHY = new YearNode;
@@ -35,17 +38,39 @@ void createSemester(YearNode*& pHY)
 		syTemp->list_sem = new SemesterNode;
 		Semester* sTemp = &(syTemp->list_sem->sem);
 		sTemp->semester_id = semester_id;
+		currY = pHY;
 	}
 	else
 	{
-		YearNode* currY = pHY;
-		while (currY->next && currY->school_year.year_id != year)
+		while (currY->next && currY->school_year.year_id != year_id)
 			currY = currY->next;
-		if (currY->school_year.year_id != year)
+		if (currY->school_year.year_id != year_id)
 		{
 			currY->next = new YearNode;
 			currY = currY->next;
-			currY->school_year.year_id = year;
+			Year* syTemp = &(currY->school_year);
+			syTemp->year_id = year_id;
+			syTemp->list_sem = new SemesterNode;
+			Semester* sTemp = &(syTemp->list_sem->sem);
+			sTemp->semester_id = semester_id;
+		}
+		else
+		{
+			SemesterNode* sNTemp = currY->school_year.list_sem;
+			while (sNTemp->next && sNTemp->sem.semester_id == semester_id)
+				sNTemp = sNTemp->next;
+			if (sNTemp->sem.semester_id != semester_id)
+			{
+				sNTemp->next = new SemesterNode;
+				sNTemp = sNTemp->next;
+				sNTemp->sem.semester_id = semester_id;
+			}
+			else
+			{
+				cout << "Semester already exists. Please try again." << endl;
+				createSemester(pHY);
+				return;
+			}
 		}
 	}
 }
