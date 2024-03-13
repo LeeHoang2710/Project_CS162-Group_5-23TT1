dc443c6f45157f9d1cdd11e97a04b64b4313c461
LeeHoang2710 <builehoang.qn@gmail.com>
2024-03-13 10:51:07 +0700

    write function of year_part

---
+++ struct and function/year.h
@@ -0,0 +1,7 @@
+#include "struct_and_library.h"
+
+Year createYear(string p_year_id);
+YearNode *initYearNode(Year school_year);
+void addNewYearNode(YearNode *&head, Year year);
+void removeNewYearNode(YearNode *&head, Year year);
+YearNode *searchYearNode(YearNode *head, Year year);
