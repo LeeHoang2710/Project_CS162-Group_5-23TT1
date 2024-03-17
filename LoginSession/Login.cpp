a2116f000968f357083bdc9c11cd8a95de11e52a
Chowkey <lekimchau211@gmail.com>
2024-03-17 14:49:04 +0700

    I finish all login session in the login session folder

---
+++ LoginSession/Login.cpp
@@ -0,0 +1,153 @@
+//Login session
+#pragma once
+#include "C:\Users\LEGION\Desktop\branches\Project_CS162\struct and function\struct_and_library.h"
+#include "LoginFunction.h" 
+#include <fstream>
+#include  <iostream>
+void StorePassWordStudent(StudentNode*& StuPass, ifstream& ip) {
+	StudentNode* cur=StuPass;
+	Student tmp;
+	getline(ip, tmp.student_id, ',');
+	getline(ip, tmp.password, '\n');
+	while (1) {
+		if (StuPass==NULL) {
+			StuPass = new StudentNode;
+			cur = StuPass;
+		}
+		else {
+			cur->next = new StudentNode;
+			cur = cur->next;
+		}
+		cur->student = tmp;
+		cur->next = NULL;
+		if (ip.eof()) return;
+		getline(ip, tmp.student_id, ',');
+		getline(ip, tmp.password, '\n');
+	}
+}
+void StorePassWordStaff(StaffNode*& StaffPass, ifstream& ip) {
+	StaffNode* cur = StaffPass;
+	Staff tmp;
+	getline(ip, tmp.username, ',');
+	getline(ip, tmp.password, '\n');
+	while (1) {
+		if (StaffPass == NULL) {
+			StaffPass = new StaffNode;
+			cur = StaffPass;
+		}
+		else {
+			cur->next = new StaffNode;
+			cur = cur->next;
+		}
+		cur->staff = tmp;
+		cur->next = NULL;
+		if (ip.eof()) return;
+		getline(ip, tmp.username, ',');
+		getline(ip, tmp.password, '\n');
+	}
+}
+void LoginForStudent(StudentNode* StuPass, string& onstatus_ID, StudentNode*& ChangeStuPass) {
+	while (1) {
+		string input_ID;
+		string input_Pass;
+		cout << "Enter your username: ";
+		cin >> input_ID;
+		cout << "Enter your password: ";
+		cin >> input_Pass;
+		StudentNode* tmp = StuPass;
+		for (tmp; tmp != NULL; tmp = tmp->next) {
+			if (tmp->student.student_id == input_ID) {
+				if (tmp->student.password == input_Pass) {
+					onstatus_ID = input_ID;
+					ChangeStuPass = tmp;
+					cout << "Login Succesfully" << endl;
+					return;
+				}
+				else {
+					cout << "wrong username or password, please try again" << endl;
+					break;
+				}
+			}
+		}
+		if (tmp==NULL) cout << "This account is not signed up yet" <<endl;
+	}
+}
+void LoginForStaff(StaffNode* StaffPass, string& onstatus_UserName, StaffNode*& ChangeStaffPass) {
+	while (1) {
+		string input_UserName;
+		string input_Pass;
+		cout << "Enter your username: ";
+		cin >> input_UserName;
+		cout << "Enter your password: ";
+		cin >> input_Pass;
+		StaffNode* tmp = StaffPass;
+		for (tmp; tmp != NULL; tmp = tmp->next) {
+			if (tmp->staff.username == input_UserName) {
+				if (tmp->staff.password == input_Pass) {
+					onstatus_UserName = input_UserName;
+					ChangeStaffPass = tmp;
+					cout << "Login Succesfully" << endl;
+					return;
+				}
+				else {
+					cout << "wrong username or password, please try again" << endl;
+					break;
+				}
+			}
+		}
+		if (tmp == NULL) cout << "This account is not signed up yet" << endl;
+	}
+}
+void UpdateStuPassFile(StudentNode* List, ofstream& op) {
+	op.open("../database/Students_Password.csv");
+	for (StudentNode* tmp = List; tmp != NULL; tmp = tmp->next) {
+		op << tmp->student.student_id <<",";
+		op << tmp->student.password;
+		if (tmp->next != NULL) op << endl;
+	}
+	op.close();
+}
+void UpdateStaffPassFile(StaffNode* List, ofstream& op) {
+	op.open("../database/Staff_Password.csv");
+	for (StaffNode* tmp = List; tmp != NULL; tmp = tmp->next) {
+		op << tmp->staff.username << ",";
+		op << tmp->staff.password ;
+		if (tmp->next != NULL) op << endl;
+	}
+	op.close();
+}
+void ChangePassStudent(StudentNode* current) {
+	while (1) {
+		string input_Pass;
+		cout << "Enter your current password: ";
+		cin >> input_Pass;
+		if (current->student.password == input_Pass) {
+			cout << "Enter your new password: ";
+			cin >> input_Pass;
+			current->student.password = input_Pass;
+			cout << "Password changed successfully " <<endl;
+			return;
+		}
+		else {
+			cout << "current password is wrong, please try again" << endl;
+		}
+	}
+}
+void ChangePassStaff(StaffNode* current) {
+	while (1) {
+		string input_Pass;
+		cout << "Enter your current password: ";
+		cin >> input_Pass;
+		if (current->staff.password == input_Pass) {
+			cout << "Enter your new password: ";
+			cin >> input_Pass;
+			current->staff.password = input_Pass;
+			cout << "Password changed successfully" << endl;
+			return;
+		}
+		else {
+			cout << "current password is wrong, please try again" << endl;
+		}
+	}
+}
+//new session
---
+++ LoginSession/LoginFunction.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "C:\Users\LEGION\Desktop\branches\Project_CS162\struct and function\struct_and_library.h"
+
+//Login Session
+void StorePassWordStudent(StudentNode*& StuPass, ifstream& ip); //These 2 fucntions will load password file to a linked list
+void StorePassWordStaff(StaffNode*& StaffPass, ifstream& ip);
+void LoginForStudent(StudentNode* StuList, string& onstatus_ID, StudentNode*& ChangeStuPass); // 1st parameter is the linked list, 2nd parameter is the ID of student who is onl
+void LoginForStaff(StaffNode* StaffPass, string& onstatus_UserName, StaffNode*& ChangeStaffPass); // 3rd parameter is the node store the current id, used it only for changing password
+void UpdateStuPassFile(StudentNode* List, ofstream& op); //Update the password file when someone change password;
+void UpdateStaffPassFile(StaffNode* List, ofstream& op);
+void ChangePassStudent(StudentNode* current); //get the node of user who is online and change password in it;
+void ChangePassStaff(StaffNode* current);
