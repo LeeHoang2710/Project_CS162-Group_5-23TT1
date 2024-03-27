cdbfc142143cb53faa55edfdf456d4b0e32d01b6
Chowkey <lekimchau211@gmail.com>
2024-03-27 09:53:41 +0700

    Fix login function, delete cin cout, and adding menu

--- SourceCode/main.cpp
+++ SourceCode/main.cpp
@@ -2,51 +2,51 @@
 #include "../struct and function/course.h"
 #include "../struct and function/student.h"
 #include "../struct and function/login.h"
-int main() {
-	//CourseNode* cshead=NULL;
-	//InputCourse(cshead);
-	////AddStudent2Course(cshead, NewCs, ip);
-	//InputCourse(cshead);
-	//displayCourse(cshead);
-	bool role;
-	cout << "enter 0 if staff, 1 if student: ";
-	cin >> role;
-	if (role) {
-		ifstream StuPassList;
-		ofstream op;
-		StuPassList.open("../database/Students_Password.csv");
-		StudentNode* StuPass = NULL;
-		StorePassWordStudent(StuPass, StuPassList);
-		string ID_Locate;
-		StudentNode* ChangeStuPass;
-		if (LoginForStudent(StuPass, ID_Locate, ChangeStuPass)) cout << "successful";
-		bool change;
-		cout << "If you want to change password, enter 1: ";
-		cin >> change;
-		if (change) {
-			ChangePassStudent(ChangeStuPass);
-			cout << ChangeStuPass->student.password << endl;
-			UpdateStuPassFile(StuPass, op);
-		}
-		StuPassList.close();
-	}
-	else {
-		ifstream StaffPassList;
-		ofstream op;
-		StaffPassList.open("../database/Staff_Password.csv");
-		StaffNode* StaffPass = NULL;
-		StorePassWordStaff(StaffPass, StaffPassList);
-		string Username_Locate;
-		StaffNode* ChangeStaffPass;
-		if(LoginForStaff(StaffPass, Username_Locate, ChangeStaffPass)) cout << "successful";
-		bool change;
-		cout << "If you want to change password, enter 1: ";
-		cin >> change;
-		if (change) {
-			ChangePassStaff(ChangeStaffPass);
-			cout << ChangeStaffPass->staff.password << endl;
-			UpdateStaffPassFile(StaffPass, op);
-		}
-		StaffPassList.close();
-	}
-}
+//int main() {
+//	//CourseNode* cshead=NULL;
+//	//InputCourse(cshead);
+//	////AddStudent2Course(cshead, NewCs, ip);
+//	//InputCourse(cshead);
+//	//displayCourse(cshead);
+//	bool role;
+//	cout << "enter 0 if staff, 1 if student: ";
+//	cin >> role;
+//	if (role) {
+//		ifstream StuPassList;
+//		ofstream op;
+//		StuPassList.open("../database/Students_Password.csv");
+//		StudentNode* StuPass = NULL;
+//		StorePassWordStudent(StuPass, StuPassList);
+//		string ID_Locate;
+//		StudentNode* ChangeStuPass;
+//		if (LoginForStudent(StuPass, ID_Locate, ChangeStuPass)) cout << "successful" <<endl;
+//		bool change;
+//		cout << "If you want to change password, enter 1: ";
+//		cin >> change;
+//		if (change) {
+//			ChangePassStudent(ChangeStuPass);
+//			cout << ChangeStuPass->student.password << endl;
+//			UpdateStuPassFile(StuPass, op);
+//		}
+//		StuPassList.close();
+//	}
+//	else {
+//		ifstream StaffPassList;
+//		ofstream op;
+//		StaffPassList.open("../database/Staff_Password.csv");
+//		StaffNode* StaffPass = NULL;
+//		StorePassWordStaff(StaffPass, StaffPassList);
+//		string Username_Locate;
+//		StaffNode* ChangeStaffPass;
+//		if(LoginForStaff(StaffPass, Username_Locate, ChangeStaffPass)) cout << "successful" << endl;
+//		bool change;
+//		cout << "If you want to change password, enter 1: ";
+//		cin >> change;
+//		if (change) {
+//			ChangePassStaff(ChangeStaffPass);
+//			cout << ChangeStaffPass->staff.password << endl;
+//			UpdateStaffPassFile(StaffPass, op);
+//		}
+//		StaffPassList.close();
+//	}
+//}
---
+++ SourceCode/menu.cpp
@@ -0,0 +1,3 @@
+#pragma once
+#include "iostream"
+#include "../struct and function/"
