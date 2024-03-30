b4c5152fdb57293194648898fec57b98f8dfcb30
ThanhPhat1509 <161999845+ThanhPhat1509@users.noreply.github.com>
2024-03-30 11:54:54 +0700

    Update function_viewscore.cpp

--- 23125088_Project/function_viewscore.cpp
+++ 23125088_Project/function_viewscore.cpp
@@ -9,17 +9,17 @@ void Viewallmyscore(Student stu)
 	cout << setw(15) << left << "Final";
 	cout << setw(15) << left << "Overall";
 	cout << "\n";
-	MyCourse* mycourse = stu.my_course;
-	while (mycourse)
+	ResultsNode* resulist = stu.results_list;
+	while (resulist)
 	{
-		cout << setw(15) << left << mycourse->subject_code;
-		cout << setw(15) << left << mycourse->year;
-		cout << setw(15) << left << mycourse->sem;
-		cout << setw(15) << left << mycourse->score.process;
-		cout << setw(15) << left << mycourse->score.midterm;
-		cout << setw(15) << left << mycourse->score.final;
-		cout << setw(15) << left <<fixed<<setprecision(1) <<  mycourse->score.overall;
-		mycourse = mycourse->next;
+		cout << setw(15) << left << resulist->results.course->course.course_id;
+		cout << setw(15) << left << resulist->results.year->school_year.year_id;
+		cout << setw(15) << left << resulist->results.sem->sem.semester_id;
+		cout << setw(15) << left << resulist->results.score.process;
+		cout << setw(15) << left << resulist->results.score.midterm;
+		cout << setw(15) << left << resulist->results.score.final;
+		cout << setw(15) << left << fixed << setprecision(1) << resulist->results.score.overall;
+		
 		cout << "\n";
 	}
 	cout << "Your curren gpa is: " << stu.cur_gpa << "\n";
@@ -29,7 +29,7 @@ void Findoverall(StudentNode* stu)
 {
 	while (stu) 
 	{
-		stu->student.my_course->score.overall = stu->student.my_course->score.final * 0.40 + stu->student.my_course->score.midterm * 0.25 + stu->student.my_course->score.process * 0.35;
+		stu->student.results_list->results.score.overall = stu->student.results_list->results.score.process * 0.35 + stu->student.results_list->results.score.midterm * 0.25 + stu->student.results_list->results.score.final * 0.35;
 		stu = stu->next;
 	}
 }
