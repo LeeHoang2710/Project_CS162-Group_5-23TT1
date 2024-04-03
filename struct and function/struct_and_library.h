d3328922d952c4e6e419bf764bb3024b6e1a10e5
ThanhPhat1509 <161999845+ThanhPhat1509@users.noreply.github.com>
2024-04-03 12:12:18 +0700

    Update struct_and_library.h

--- struct and function/struct_and_library.h
+++ struct and function/struct_and_library.h
@@ -1,127 +1,137 @@
 #pragma once
 #include <iostream>
 #include <fstream>
-#include <string.h>
+#include <cstring>
+#include <string>
 #include <sstream>
-#include <time.h>
+#include <ctime>
 
 using namespace std;
 
-struct Year;
-struct YearNode;
-struct Class;
-struct ClassNode;
-struct Course;
-struct CourseNode;
-struct MyCourse;
-struct Semester;
-struct SemesterNode;
+struct Score;
+struct Results;
+struct ResultsNode;
+
 struct Student;
 struct StudentNode;
+
+struct Class;
+struct ClassNode;
+
 struct Session;
-struct Score;
+struct Course;
+struct CourseNode;
 
-struct Year
-{
-    string year_id;
-    SemesterNode *list_sem = nullptr;
-};
-struct YearNode
-{
-    Year school_year;
-    YearNode *next;
-};
+struct Semester;
+struct SemesterNode;
 
-struct Class
+struct Year;
+struct YearNode;
+
+struct Score
 {
-    string class_id;
-    string head_teacher;
-    StudentNode *student_list = nullptr;
-};
-struct ClassNode
-{
-    Class my_class;
-    ClassNode *next;
+	float process = 0.0f;
+	float midterm = 0.0f;
+	float final = 0.0f;
+	float overall = 0.0f;
 };
 
-struct Session
+struct Results
 {
-    int day_of_the_week; // MON / TUE / WED / THU / FRI / SAT
-    int session_no;      // S1 (07:30), S2 (09:30), S3(13:30) and S4 (15:30)
-};
-struct Course
-{
-    string course_id;
-    string course_name;
-    string teacher_name;
-    int num_credit;
-    int max_students = 50;
-    Session teaching_session[2];
-};
-struct CourseNode
-{
-    string semester_id;
-    string year_id;
-    Course course;
-    StudentNode *student_list = nullptr;
-    CourseNode *next;
-};
-struct MyCourse
-{
-    Score score;
-    string subject_code;
-    string sem;
-    string year;
-    CourseNode *course = nullptr;
-    MyCourse *next;
+	Score score;
+	CourseNode* course = nullptr;
+	SemesterNode* sem = nullptr;
+	YearNode* year = nullptr;
 };
 
-struct Semester
+struct ResultsNode
 {
-    string start_day;
-    string end_day;
-    string semester_id;
-    CourseNode *course_list = nullptr;
-};
-struct SemesterNode
-{
-    Semester sem;
-    SemesterNode *next;
-};
-struct RegistrationSession
-{
-    string year;
-    string sem;
-    CourseNode *list_of_courses = nullptr;
-    string start_date;
-    string end_date;
+	Results results;
+	ResultsNode* next = nullptr;
 };
 
 struct Student
 {
-    int num;
-    string student_id;
-    string first_name;
-    string last_name;
-    bool gender; // male(0), female(1)
-    string dob;
-    string social_id;
-    string password = "default";
-    string student_class;
-    MyCourse *my_course = nullptr;
-    float cur_gpa = 0.0f;
-    float total_gpa = 0.0f;
-};
-struct StudentNode
-{
-    Student student;
-    StudentNode *next;
+	int num;
+	string student_id;
+	string first_name;
+	string last_name;
+	bool gender; // male(0), female(1)
+	string dob;
+	string social_id;
+	string password = "123456";
+	string student_class;
+	ResultsNode* results_list = nullptr;
+	float cur_gpa = 0.0f;
+	float total_gpa = 0.0f;
 };
 
-struct Score
+struct StudentNode
 {
-    float process = 0.0f;
-    float midterm = 0.0f;
-    float final = 0.0f;
-    float overall = 0.0f;
+	Student student;
+	StudentNode* next = nullptr;
+};
+
+struct Class
+{
+	string class_id;
+	string head_teacher;
+	StudentNode* student_list = nullptr;
+};
+
+struct ClassNode
+{
+	Class my_class;
+	ClassNode* next = nullptr;
+};
+
+struct Session
+{
+	int day_of_the_week; // MON / TUE / WED / THU / FRI / SAT
+	int session_no;      // S1 (07:30), S2 (09:30), S3(13:30) and S4 (15:30)
+};
+
+struct Course
+{
+	string course_id;
+	string course_name;
+	string teacher_name;
+	int num_credit;
+	int max_students = 50;
+	Session teaching_session;
+};
+
+struct CourseNode
+{
+	string semester_id;
+	string year_id;
+	Course course;
+	StudentNode* student_list = nullptr;
+	CourseNode* next = nullptr;
+};
+
+struct Semester
+{
+	string semester_id;
+	string start_date;
+	string end_date;
+	CourseNode* course_list = nullptr;
+};
+
+struct SemesterNode
+{
+	Semester sem;
+	SemesterNode* next = nullptr;
+};
+
+struct Year
+{
+	string year_id;
+	SemesterNode* list_sem = nullptr;
+};
+
+struct YearNode
+{
+	Year school_year;
+	YearNode* next = nullptr;
 };
