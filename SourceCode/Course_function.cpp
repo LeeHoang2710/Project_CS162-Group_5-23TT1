7d120ad38d87936ed12acdc9ae9982f4acd8de23
Chowkey <lekimchau211@gmail.com>
2024-04-13 10:14:33 +0700

    Add class to course

--- SourceCode/Course_function.cpp
+++ SourceCode/Course_function.cpp
@@ -142,7 +142,7 @@ void exportCourse(CourseNode* Courselist, string filename, ofstream& fout) {
 Results createResults(string year_cur, string sem_cur, string cour_cur) {
 	Results tmp;
 	tmp.year_id = year_cur;
-	tmp.semester_id = sem_cur;
+	tmp.sem_id= sem_cur;
 	tmp.course_id = cour_cur;
 	return tmp;
 }
@@ -170,7 +170,160 @@ void CourseEnroll(StudentNode*& student_list, CourseNode* curr) {
 	if (student_list == nullptr) return;
 	for (StudentNode* tmp = student_list; tmp != NULL; tmp = tmp->next) {
 		Results res = createResults(curr->year_id, curr->semester_id, curr->course.course_id);
-		appendNewResultsNode(tmp->student.my_course, res);
+		appendNewResultsNode(tmp->student.results_list, res);
 	}
 }
 //void ViewCourses(StudentNode* )
+void Loadcoursescorefromfile(ifstream& fin, StudentNode*& Studentlist, CourseNode* curr)
+{
+	StudentNode* tmp1 = nullptr;
+	string Line;
+	while (getline(fin, Line, '\n') && Line != "")
+	{
+		if (Line != "*")
+		{
+			StudentNode* tmp = nullptr;
+			if (!Studentlist)
+			{
+				Studentlist = new StudentNode();
+				Studentlist->next = nullptr;
+				stringstream ss1(Line);
+				string s1;
+				getline(ss1, s1, ',');
+				Studentlist->student.student_id = s1;
+				getline(ss1, s1, ',');
+				Studentlist->student.first_name = s1;
+				getline(ss1, s1, ',');
+				Studentlist->student.last_name = s1;
+				getline(ss1, s1, '\n');
+				Studentlist->student.dob = s1;
+				Studentlist->student.student_class = curr->course.main_class.class_id;
+				tmp1 = Studentlist;
+				tmp = Studentlist;
+			}
+			else
+			{
+				StudentNode* newstudent = new StudentNode();
+				newstudent->next = nullptr;
+				stringstream ss1(Line);
+				string s1;
+				getline(ss1, s1, ',');
+				newstudent->student.student_id = s1;
+				getline(ss1, s1, ',');
+				newstudent->student.first_name = s1;
+				getline(ss1, s1, ',');
+				newstudent->student.last_name = s1;
+				tmp1->next = newstudent;
+				tmp1 = tmp1->next;
+				tmp = newstudent;
+			}
+			ResultsNode* Resulist = nullptr;
+			ResultsNode* tmp2 = nullptr;
+			while (getline(fin, Line, '\n'))
+			{
+				if (Line != "*")
+				{
+					if (!Resulist)
+					{
+						Resulist = new ResultsNode();
+						Resulist->next = nullptr;
+						stringstream ss2(Line);
+						string s2;
+						getline(ss2, s2, ',');
+						Resulist->results.course_id = s2;
+						getline(ss2, s2, ',');
+						Resulist->results.year_id = s2;
+						getline(ss2, s2, ',');
+						Resulist->results.sem_id = s2;
+						float fl;
+						getline(ss2, s2, ',');
+						if (s2.size() == 3)
+						{
+							fl = (float)(s2[0] - '0') + (float)(s2[2] - '0') / 10;
+							Resulist->results.score.process = fl;
+						}
+						else
+							Resulist->results.score.process = (float)(s2[0] - '0');
+						getline(ss2, s2, ',');
+						if (s2.size() == 3)
+						{
+							fl = (float)(s2[0] - '0') + (float)(s2[2] - '0') / 10;
+							Resulist->results.score.midterm = fl;
+						}
+						else
+							Resulist->results.score.midterm = (float)(s2[0] - '0');
+						getline(ss2, s2, ',');
+						if (s2.size() == 3)
+						{
+							fl = (float)(s2[0] - '0') + (float)(s2[2] - '0') / 10;
+							Resulist->results.score.final = fl;
+						}
+						else
+							Resulist->results.score.final = (float)(s2[0] - '0');
+						Resulist->results.score.overall = 0.35 * Resulist->results.score.process + 0.25 * Resulist->results.score.midterm + 0.4 * Resulist->results.score.final;
+						tmp2 = Resulist;
+					}
+					else
+					{
+						ResultsNode* newResulist = new ResultsNode();
+						newResulist->next = nullptr;
+						stringstream ss2(Line);
+						string s2;
+						getline(ss2, s2, ',');
+						newResulist->results.course_id = s2;
+						getline(ss2, s2, ',');
+						newResulist->results.year_id = s2;
+						getline(ss2, s2, ',');
+						newResulist->results.sem_id = s2;
+						float fl;
+						getline(ss2, s2, ',');
+						if (s2.size() == 3)
+						{
+							fl = (float)(s2[0] - '0') + (float)(s2[2] - '0') / 10;
+							newResulist->results.score.process = fl;
+						}
+						else
+							newResulist->results.score.process = (float)(s2[0] - '0');
+						getline(ss2, s2, ',');
+						if (s2.size() == 3)
+						{
+							fl = (float)(s2[0] - '0') + (float)(s2[2] - '0') / 10;
+							newResulist->results.score.midterm = fl;
+						}
+						else
+							newResulist->results.score.midterm = (float)(s2[0] - '0');
+						getline(ss2, s2, ',');
+						if (s2.size() == 3)
+						{
+							fl = (float)(s2[0] - '0') + (float)(s2[2] - '0') / 10;
+							newResulist->results.score.final = fl;
+						}
+						else
+							newResulist->results.score.final = (float)(s2[0] - '0');
+						newResulist->results.score.overall = 0.35 * newResulist->results.score.process + 0.25 * newResulist->results.score.midterm + 0.4 * newResulist->results.score.final;
+						tmp2->next = newResulist;
+						tmp2 = tmp2->next;
+					}
+				}
+				else
+					break;
+			}
+			tmp->student.results_list = Resulist;
+		}
+	}
+}
+bool addmainclass(CourseNode*& curr, string class_id, ifstream& ip) {
+	ip.open("class.csv");
+	Course temp;
+	while (!ip.eof()) {
+		getline(ip, temp.main_class.class_id, '\n');
+		Loadcoursescorefromfile(ip, temp.main_class.student_list, curr);
+		if (temp.main_class.class_id == class_id) {
+			curr->course = temp;
+		}
+		ip.close();
+		return true;
+	}
+	ip.close();
+	return false;
+}
